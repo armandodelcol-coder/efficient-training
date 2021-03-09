85b010fd4ee3996b2b4278ac419cdb91d7ffdd5d
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-03-09 14:06:39 -0300

    initial commit

---
+++ src/main/java/br/com/armando/efficienttraining/EfficientTrainingApplication.java
@@ -0,0 +1,13 @@
+package br.com.armando.efficienttraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfficientTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfficientTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/armando/efficienttraining/EfficientTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.armando.efficienttraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficientTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
